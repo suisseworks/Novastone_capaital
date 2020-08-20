c3aebe793a8e7bdc3f39e1ac34f5f1d8dfe9fb41
edrick27 <edricklpcr@gmail.com>
2020-08-19 22:16:43 -0600

    init proyect

---
+++ capital.js
@@ -0,0 +1,8 @@
+function goToWebsite() {
+	window.location.href = "http://novastone-ca.com/";
+}
+
+function openContactEmail() {
+	window.location.href = "https://mail.google.com/mail/?view=cm&fs=1&tf=1&to=contact@novastonecapital.com";
+	// window.location.href = "mailto:contact@novastonecapital.com";
+}
